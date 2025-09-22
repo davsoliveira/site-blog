340767ef1b43d62e5d0aaa1229cba94fc6a14d66
Davi Oliveira <davi.ol.santos@gmail.com>
2025-09-22 08:34:38 -0300

    feat: criando style guide

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export function Logo() {
   return (
     <Link href="/" title="Página inicial">
-      <Image src="/logo.svg" alt="Logo site" width={116} height={32} />
+      <Image src="/logo.png" alt="Logo site" width={116} height={32} />
     </Link>
   );
 };
