50b0e184260c8be114e595356cc56c719b855f02
Davi Oliveira <davi.ol.santos@gmail.com>
2025-10-17 08:59:30 -0300

    refactor: aplicando ssg no blog

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,22 +1,26 @@
 import { useRouter } from "next/router";
 import { PostCard } from "./components/post-card";
 import { PostGridCard } from "./components/post-grid-card";
-import { allPosts } from "contentlayer/generated";
+import { Post } from "contentlayer/generated";
 import { Inbox } from "lucide-react";
 import { Search } from "@/components/search";
 
-export function BlogList() {
+export type BlogListProps = {
+  posts: Post[]
+}
+
+export function BlogList({ posts }: BlogListProps) {
   const router = useRouter();
   const query = router.query.q as string;
   const pageTitle = query
     ? `Resultados de busca para "${query}"`
     : 'Dicas e estratégias para impulsionar seu negócio';
 
-  const posts = query
-    ? allPosts.filter((post) =>
-        post.title.toLocaleLowerCase()?.includes(query.toLocaleLowerCase())
-      )
-    : allPosts;
+  const postList = query
+    ? posts.filter((post) =>
+      post.title.toLocaleLowerCase()?.includes(query.toLocaleLowerCase())
+    )
+    : posts;
 
   const hasPosts = posts?.length > 0;
 
@@ -40,7 +44,7 @@ export function BlogList() {
 
       {hasPosts && (
         <PostGridCard>
-          {posts.map((post) => (
+          {postList.map((post) => (
             <PostCard
               key={post._id}
               title={post.title}
