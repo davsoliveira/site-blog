4f4fef9f3cef0fa59bef15a418620049c0d52f13
Davi Oliveira <davi.ol.santos@gmail.com>
2025-11-10 08:33:21 -0300

    chore: adicionar seo na página de listagem de posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,33 @@
+import type { Metadata } from "next";
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
 
-export default function BlogListPage() {
-  const sortedPosts = allPosts.sort((a,b)=> new Date(b.date).getDate() - new Date(a.date).getTime())
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar seu negócio",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Dicas e estratégias para impulsionar seu negócio",
+    url: "https://nextjs-fundamentos.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt-BR",
+    type: "website",
+    images: [
+      {
+        url: "https://nextjs-fundamentos.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 
-  return (
-    <BlogList posts={sortedPosts} />
-  )
+export default function BlogListPage() {
+  const sortedPosts = allPosts.sort(
+    (a, b) => new Date(b.date).getDate() - new Date(a.date).getTime()
+  );
+
+  return <BlogList posts={sortedPosts} />;
 }
